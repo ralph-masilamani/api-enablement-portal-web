0bec768ecea9a1989f337971125ad6960fe70c93
vmmgome <daniel.costin@valueblock.io>
2019-04-18 13:04:21 +0100

    refactor env

--- src/model/Environment.tsx
+++ src/model/Environment.tsx
@@ -1,27 +1,24 @@
 
-
 export class EnvConfig {
 
+    public apiUrl: any
+    public homeUri: any
     public baseApiUrl: string
 
-    constructor(
-        public apiUrl: any,
-        public homeUri: any,
-       
-    ) {
-        console.log('configured env api url', this.apiUrl)
-        console.log('configured env home uri', this.homeUri)
+    constructor() {
+        this.apiUrl = process.env.REACT_APP_BASE_API_URL;
+        this.homeUri = process.env.REACT_APP_PLATFORM_HOME_URI;
+        console.log('configured env api url ', this.apiUrl)
+        console.log('configured env home uri ', this.homeUri)
         if (this.apiUrl && this.homeUri) {
             this.baseApiUrl = this.apiUrl + this.homeUri;
         } else {
             this.baseApiUrl = 'NOT SET'
+            throw new Error('Unable to start application as env variables REACT_APP_BASE_API_URL and REACT_APP_PLATFORM_HOME_URI are not set' )
         }
     }
 }
 
-const environment: EnvConfig = new EnvConfig(
-    process.env.REACT_APP_BASE_API_URL,
-    process.env.REACT_APP_PLATFORM_HOME_URI
-)
+const environment: EnvConfig = new EnvConfig()
 
 export default environment;
